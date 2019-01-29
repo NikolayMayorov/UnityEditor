54efa3f1435a66c500e21fcb05e0d830c61c28bc
Mayorov Nikolay <slidings@mail.ru>
2019-01-29 19:52:03 +0300

    Add scripts

---
+++ HeroParamEditor.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class HeroParamEditor : EditorWindow
+{
+    private HeroParam _playerParam;
+
+    private GUIStyle _styleHeader = new GUIStyle();
+
+    private GUIStyle _styleSubHeader = new GUIStyle();
+
+    private bool _showMainSkill = false;
+
+    private bool _showOtherSkill = false;
+
+    [MenuItem("Tower Defence/Hero param")]
+    public static void CreateWindow()
+    {
+        EditorWindow.GetWindow<HeroParamEditor>();
+    }
+
+    private void OnGUI()
+    {
+        _styleHeader.fontStyle = FontStyle.Bold;
+
+
+        _styleSubHeader.fontStyle = FontStyle.Bold;
+
+
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.LabelField("Профайлер с настройками героя", _styleHeader, GUILayout.Width(400));
+        _playerParam = EditorGUILayout.ObjectField(_playerParam, typeof(HeroParam), true, GUILayout.Width(400)) as HeroParam;
+        EditorGUILayout.EndHorizontal();
+       
+        if (_playerParam != null)
+        {
+            #region Основные способности героя Rage, Curse, Star
+            EditorGUILayout.Separator();
+    
+            //EditorGUILayout.BeginHorizontal();
+            //EditorGUILayout.LabelField("Показать основные способности героя", GUILayout.Width(300));
+            //_showMainSkill = EditorGUILayout.Toggle( _showMainSkill);
+            //EditorGUILayout.EndHorizontal();
+
+           // if (_showMainSkill)
+            {
+                EditorGUILayout.LabelField("Активные способности героя", _styleHeader);
+                EditorGUILayout.Separator();
+
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.LabelField("Время отката - Бешенство", GUILayout.Width(300));
+                _playerParam.skillTwoCooldownTimerValue = EditorGUILayout.DelayedFloatField( _playerParam.skillTwoCooldownTimerValue, GUILayout.Width(400));
+                EditorGUILayout.EndHorizontal();
+
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.LabelField("Время отката - Проклятая земля", GUILayout.Width(300));
+                _playerParam.skillThreeCooldownTimerValue = EditorGUILayout.DelayedFloatField(_playerParam.skillThreeCooldownTimerValue, GUILayout.Width(400));
+                EditorGUILayout.EndHorizontal();
+
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.LabelField("Время отката - Взрыв", GUILayout.Width(300));
+                _playerParam.skillFourCooldownTimerValue = EditorGUILayout.DelayedFloatField(_playerParam.skillFourCooldownTimerValue, GUILayout.Width(400));
+                EditorGUILayout.EndHorizontal();
+
+            }
+
+            #endregion
+
+
+            //EditorGUILayout.BeginHorizontal();
+            //EditorGUILayout.LabelField("Показать остальные способности героя", GUILayout.Width(300));
+            //_showOtherSkill = EditorGUILayout.Toggle(_showOtherSkill);
+            //EditorGUILayout.EndHorizontal();
+
+           // if (_showOtherSkill)
+            {
+                #region TriggerSkill
+                EditorGUILayout.Separator();
+                EditorGUILayout.Separator();
+                EditorGUILayout.Separator();
+                EditorGUILayout.LabelField("Триггерные навыки героя", _styleHeader);
+                EditorGUILayout.LabelField("Шрапнель", _styleSubHeader);
+                _playerParam.cooldownTimerValueFlak = EditorGUILayout.DelayedFloatField("Время отката ", _playerParam.cooldownTimerValueFlak);
+                _playerParam.probabilityFlak = EditorGUILayout.DelayedFloatField("Probability ", _playerParam.probabilityFlak);
+
+               // EditorGUILayout.Separator();
+                EditorGUILayout.LabelField("Огненная волна", _styleSubHeader);
+                _playerParam.cooldownTimerValueFlame = EditorGUILayout.DelayedFloatField("Время отката ", _playerParam.cooldownTimerValueFlame);
+                _playerParam.probabilityFlame = EditorGUILayout.DelayedFloatField("Probability ", _playerParam.probabilityFlame);
+
+              //  EditorGUILayout.Separator();
+                EditorGUILayout.LabelField("Иглы", _styleSubHeader);
+                _playerParam.cooldownTimerValueNails = EditorGUILayout.DelayedFloatField("Время отката ", _playerParam.cooldownTimerValueNails);
+                _playerParam.probabilityNails = EditorGUILayout.DelayedFloatField("Probability ", _playerParam.probabilityNails);
+                #endregion
+
+                #region KillSkill
+                EditorGUILayout.Separator();
+                EditorGUILayout.Separator();
+                EditorGUILayout.Separator();
+                EditorGUILayout.LabelField("Навыки убийства героя", _styleHeader);
+                EditorGUILayout.LabelField("Мина", _styleHeader);
+                _playerParam.killCountValueMine = EditorGUILayout.DelayedFloatField("Kill count ", _playerParam.killCountValueMine);
+                EditorGUILayout.LabelField("Гейзер", _styleHeader);
+                _playerParam.killCountValueGazer = EditorGUILayout.DelayedFloatField("Kill count ", _playerParam.killCountValueGazer);     
+                EditorGUILayout.LabelField("Кража энергии", _styleHeader);
+                _playerParam.killCountValueRefresh = EditorGUILayout.DelayedFloatField("Kill count ", _playerParam.killCountValueRefresh);
+                #endregion
+            }
+        }
+
+
+        if (GUI.changed)
+            EditorUtility.SetDirty(_playerParam);
+    }
+
+}
---
+++ SpawnEditor.cs
@@ -0,0 +1,191 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class SpawnEditor : EditorWindow
+{
+    private SpawnProfiler _spawnProfiler;
+
+    private WayProfiler _wayProfiler;
+
+    private AvailableSpawnObjectsProfiler _availableSpawnObjectsProfiler;
+
+    private GUIStyle _styleHeader = new GUIStyle();
+
+    private GUIStyle _styleHeaderWave = new GUIStyle();
+
+    private string[] _wayList;
+
+    private bool _showSetWay;
+
+    private Vector2 scrollView;
+
+
+
+    int countTest;
+
+    GameObject gameObject = null;
+
+    [MenuItem("Tower Defence/Spawn Editor")]
+    public static void CreateWindow()
+    {
+        EditorWindow.GetWindow<SpawnEditor>();
+    }
+
+    private void OnGUI()
+    {
+        _styleHeader.fontStyle = FontStyle.Bold;
+        _styleHeaderWave.fontStyle = FontStyle.Bold;
+       
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.LabelField("Спавн профайлер", _styleHeader, GUILayout.Width(300));
+        _spawnProfiler = EditorGUILayout.ObjectField(_spawnProfiler, typeof(SpawnProfiler), true, GUILayout.Width(400)) as SpawnProfiler;
+        EditorGUILayout.EndHorizontal();
+        EditorGUILayout.Space();
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.LabelField("Профайлер путей", _styleHeader, GUILayout.Width(300));
+        _wayProfiler = EditorGUILayout.ObjectField(_wayProfiler, typeof(WayProfiler), true, GUILayout.Width(400)) as WayProfiler;
+        EditorGUILayout.EndHorizontal();
+        EditorGUILayout.Space();
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.LabelField("Профайлер доступных объектов для спавна", _styleHeader, GUILayout.Width(300));
+        _availableSpawnObjectsProfiler = EditorGUILayout.ObjectField(_availableSpawnObjectsProfiler, typeof(AvailableSpawnObjectsProfiler), true, GUILayout.Width(400)) as AvailableSpawnObjectsProfiler;
+        EditorGUILayout.EndHorizontal();
+        EditorGUILayout.Space();
+        if (_spawnProfiler && _wayProfiler && _availableSpawnObjectsProfiler)
+        {
+
+            //otladka
+            _spawnProfiler.availableSpawn = _availableSpawnObjectsProfiler;
+            //_spawnProfiler.InitWave(HelperSpawn.countWave);
+            //_spawnProfiler.InitWave(_spawnProfiler.WAVE_COUNT_FROM_INSPECTOR);
+            //otladka
+
+            #region Ввод основных параметров
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("Номер левела", _styleHeader, GUILayout.Width(300));
+            _spawnProfiler.levelNumber = EditorGUILayout.IntField(_spawnProfiler.levelNumber, GUILayout.Width(100));
+            EditorGUILayout.EndHorizontal();
+            EditorGUILayout.Space();
+            EditorGUILayout.BeginHorizontal();
+            //EditorGUILayout.LabelField("Количество волн на левеле   " + _spawnProfiler.waveInfo.waveCount, _styleHeader, GUILayout.Width(300));
+            EditorGUILayout.LabelField("Количество волн на левеле   " , _styleHeader, GUILayout.Width(300));
+            int tempWaveCount = EditorGUILayout.DelayedIntField(_spawnProfiler.waveInfo.waveCount, GUILayout.Width(100));
+            if (_spawnProfiler.waveInfo.waveCount != tempWaveCount)
+            {
+                if (!_spawnProfiler.InitWave(tempWaveCount))
+                    Debug.LogError("Недопустимое значение для изменения количества волн");
+            }
+            EditorGUILayout.EndHorizontal();
+            EditorGUILayout.Space();
+
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("Минимальная задержка спавна объекта", _styleHeader, GUILayout.Width(300));
+            _spawnProfiler._minDelaySpawn = EditorGUILayout.FloatField(_spawnProfiler._minDelaySpawn, GUILayout.Width(100));
+            EditorGUILayout.EndHorizontal();
+            EditorGUILayout.Space();
+
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("Максимальная задержка спавна объекта", _styleHeader, GUILayout.Width(300));
+            _spawnProfiler._maxDelaySpawn = EditorGUILayout.FloatField(_spawnProfiler._maxDelaySpawn, GUILayout.Width(100));
+            EditorGUILayout.EndHorizontal();
+            EditorGUILayout.Space();
+
+  
+
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("Количество спавн поинтов на левеле", _styleHeader, GUILayout.Width(300));
+            int tempSpawnPointCount = EditorGUILayout.DelayedIntField(_spawnProfiler.spawnPointCount, GUILayout.Width(100));
+            if (_spawnProfiler.spawnPointCount != tempSpawnPointCount)
+            {
+                if (!_spawnProfiler.ResizeSpawnPointCount(tempSpawnPointCount))
+                    Debug.LogError("Недопустимое значение для изменения количества спавн поинтов");
+            }
+
+
+            EditorGUILayout.EndHorizontal();
+            EditorGUILayout.Space();
+#endregion
+
+            _wayList = new string[_wayProfiler.GetCountWay()];
+            _wayList = _wayProfiler.GetNamesWays();
+
+            _showSetWay = EditorGUILayout.Foldout(_showSetWay, "Выбор пути для спавн поинтов");
+            if (_showSetWay)
+            {
+                for (int j = 0; j < _spawnProfiler.spawnPointCount; j++)
+                {
+                    EditorGUILayout.BeginHorizontal();
+                    EditorGUILayout.LabelField("Спавн поинт № " + (j + 1), GUILayout.Width(300));
+                    int selectedWayIndex = EditorGUILayout.Popup(_spawnProfiler.spawnDatas[j].selectedWayIndex, _wayList);
+                    EditorGUILayout.EndHorizontal();
+                    _spawnProfiler.spawnDatas[j].namePath = _wayList[selectedWayIndex];
+                    _spawnProfiler.spawnDatas[j].selectedWayIndex = selectedWayIndex;
+                    _spawnProfiler.spawnDatas[j].path = _wayProfiler.GetPointsWay(_spawnProfiler.spawnDatas[j].namePath);
+                }
+            }
+            EditorGUILayout.Space();
+            EditorGUILayout.Space();
+            EditorGUILayout.Space();
+            EditorGUILayout.Space();
+            scrollView = EditorGUILayout.BeginScrollView(scrollView);
+            for (int indexWaveNumber = 0; indexWaveNumber < _spawnProfiler.waveInfo.waveCount; indexWaveNumber++)
+            {
+                EditorGUILayout.LabelField("Волна № " + (indexWaveNumber + 1), _styleHeader, GUILayout.Width(300));
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.LabelField("Секунд до волны", GUILayout.Width(300));
+                _spawnProfiler.waveInfo.timeBeforeWave[indexWaveNumber] = EditorGUILayout.DelayedIntField(_spawnProfiler.waveInfo.timeBeforeWave[indexWaveNumber], GUILayout.Width(100));
+                EditorGUILayout.EndHorizontal();
+
+
+
+                for (int indexSpawnPoint = 0; indexSpawnPoint < _spawnProfiler.spawnPointCount; indexSpawnPoint++)
+                {
+                    EditorGUILayout.LabelField("    Спавн поинт № " + (indexSpawnPoint + 1), _styleHeader, GUILayout.Width(300));
+                 
+
+                    for (int indexAvalaibleSpawnObjects = 0; indexAvalaibleSpawnObjects < _availableSpawnObjectsProfiler.spawnObjects.Count; indexAvalaibleSpawnObjects++)
+                    {
+                        //GameObject temp;
+                        int tempV = 1;
+                        EditorGUILayout.BeginHorizontal();
+                        //  temp = EditorGUILayout.ObjectField(_availableSpawnObjectsProfiler.spawnObjects[indexAvalaibleSpawnObjects], typeof(GameObject), true, GUILayout.Width(500)) as GameObject;
+                        if (_availableSpawnObjectsProfiler.spawnObjects[indexAvalaibleSpawnObjects] != null)
+                        {
+                            EditorGUILayout.LabelField("    " + _availableSpawnObjectsProfiler.spawnObjects[indexAvalaibleSpawnObjects].name, GUILayout.Width(300));
+                            tempV = EditorGUILayout.DelayedIntField(_spawnProfiler.spawnDatas[indexSpawnPoint].spawnPointDatas[indexWaveNumber].countForSpawn[indexAvalaibleSpawnObjects]);
+                            _spawnProfiler.spawnDatas[indexSpawnPoint].spawnPointDatas[indexWaveNumber].countForSpawn[indexAvalaibleSpawnObjects] = tempV;
+                        }
+
+                        EditorGUILayout.EndHorizontal();
+                    }
+
+
+              
+                    EditorGUILayout.Space();
+                    EditorGUILayout.Space();
+                }
+
+
+
+                EditorGUILayout.Space();
+                EditorGUILayout.Space();
+                EditorGUILayout.Space();
+                EditorGUILayout.Space();
+
+
+            }
+
+
+
+            EditorGUILayout.EndScrollView();
+        }
+
+        if (GUI.changed)
+            EditorUtility.SetDirty(_spawnProfiler);
+
+    }
+}
+
+
